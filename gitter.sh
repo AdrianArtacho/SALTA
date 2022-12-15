6a185e207d5ec182f51bac98edc95acf6fa44ae1
Adrian Artacho <adrian.artacho@gmail.com>
2022-12-15 19:03:11 +0100

    pushing everything

--- gitter.sh
+++ gitter.sh
@@ -1,8 +1,9 @@
 #!/bin/bash         
 
 echo "Starting the gitter shell script..."
-git submodule foreach 'git status || :'
-git submodule foreach 'git add . || :' 
+#git submodule foreach 'git status || :'
+#git submodule foreach 'git add . || :' 
+git --no-optional-locks -c color.branch=false -c color.diff=false -c color.status=false -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree add .
 git submodule foreach 'git commit -m everything || :' 
 git submodule foreach 'git push origin master || :' 
 git add . 
